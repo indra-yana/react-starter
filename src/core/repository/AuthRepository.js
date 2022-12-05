cbcedb9c3afd31b77a4762743652eb16c844e86e
DESKTOP-FMVAQ3E\Indra QW <indra@qwords.co.id>
2022-12-05 14:39:17 +0700

    Add more function

--- src/core/repository/AuthRepository.js
+++ src/core/repository/AuthRepository.js
@@ -1,5 +1,14 @@
 import { error, success } from "../../utils/response";
-import { login, register } from "../datasource/remote/api/auth-api";
+import { 
+    login, 
+    logout, 
+    register, 
+    resetPassword, 
+    sendResetPasswordLink, 
+    sendVerificationLink, 
+    verify, 
+    whoami 
+} from "../datasource/remote/api/auth-api";
 
 export class AuthRepository {
     async login(credential, password) {
@@ -8,8 +17,46 @@ export class AuthRepository {
             .catch(error);
     }
 
+    async logout() {
+        return await logout();
+    }
+
     async register(payloads) {
         return await register(payloads)
+            .then(success)
+            .catch(error);
+    }
+
+    async sendVerificationLink(email) {
+        return await sendVerificationLink(email)
+            .then(success)
+            .catch(error);
+    }
+
+    async verify(token, email) {
+        return await verify(token, email)
+    }
+
+    async sendVerificationLink(email) {
+        return await sendResetPasswordLink(email)
+            .then(success)
+            .catch(error);
+    }
+
+    async resetPassword(password, password_confirmation) {
+        return await resetPassword(password, password_confirmation)
+            .then(success)
+            .catch(error);
+    }
+
+    async refreshToken(refreshToken) {
+        return await refreshToken(refreshToken)
+            .then(success)
+            .catch(error);
+    }
+
+    async whoami() {
+        return await whoami()
             .then(success)
             .catch(error);
     }
