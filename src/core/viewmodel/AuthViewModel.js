99b675934804b4111a704b07b6002649a59e82bc
Indra Muliana <indra.ndra26@gmail.com>
2022-12-06 21:21:55 +0700

    Viewmodel function

--- src/core/viewmodel/AuthViewModel.js
+++ src/core/viewmodel/AuthViewModel.js
@@ -1,6 +1,6 @@
 import { RepositoryFactory } from "../repository/RepositoryFactory";
-import { useState } from "react";
 import { STATE } from "../../utils/view-state";
+import { useState } from "react";
 
 const authRepository = RepositoryFactory.get('auth');
 
@@ -11,6 +11,19 @@ export function AuthViewModel() {
         setState(STATE.Loading);
         
         const result = await authRepository.sendResetPasswordLink(email);
+
+        if (result.status === 'error') {
+            setState(STATE.Error(result));
+            return;
+        }
+
+        setState(STATE.Success(result));
+    }
+
+    async function resetPassword(password, password_confirmation) {
+        setState(STATE.Loading);
+        
+        const result = await authRepository.resetPassword(password, password_confirmation);
 
         if (result.status === 'error') {
             setState(STATE.Error(result));
@@ -33,6 +46,19 @@ export function AuthViewModel() {
         setState(STATE.Success(result));
     }
 
+    async function logout() {
+        setState(STATE.Loading);
+        
+        const result = await authRepository.logout();
+
+        if (result.status === 'error') {
+            setState(STATE.Error(result));
+            return;
+        }
+
+        setState(STATE.Success(result));
+    }
+
     async function register(payloads) {
         setState(STATE.Loading);
         
@@ -45,11 +71,69 @@ export function AuthViewModel() {
 
         setState(STATE.Success(result));
     }
+    
+    async function sendVerificationLink(email) {
+        setState(STATE.Loading);
+        
+        const result = await authRepository.sendVerificationLink(email);
+
+        if (result.status === 'error') {
+            setState(STATE.Error(result));
+            return;
+        }
+
+        setState(STATE.Success(result));
+    }
+
+    async function verify(token, email) {
+        setState(STATE.Loading);
+        
+        const result = await authRepository.verify(token, email);
+
+        if (result.status === 'error') {
+            setState(STATE.Error(result));
+            return;
+        }
+
+        setState(STATE.Success(result));
+    }
+
+    async function refreshToken(refreshToken) {
+        setState(STATE.Loading);
+        
+        const result = await authRepository.refreshToken(refreshToken);
+
+        if (result.status === 'error') {
+            setState(STATE.Error(result));
+            return;
+        }
+
+        setState(STATE.Success(result));
+    }
+
+    async function whoami() {
+        setState(STATE.Loading);
+        
+        const result = await authRepository.whoami();
+
+        if (result.status === 'error') {
+            setState(STATE.Error(result));
+            return;
+        }
+
+        setState(STATE.Success(result));
+    }
 
     return [
         state,
         login,
+        logout,
         register,
         sendResetPasswordLink,
+        resetPassword,
+        sendVerificationLink,
+        verify,
+        refreshToken,
+        whoami,
     ];
 }
