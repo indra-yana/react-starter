0ad8b97f56b6cc137ae485f221caba6545cc2653
Indra Muliana <indra.ndra26@gmail.com>
2022-12-21 23:14:09 +0700

    Update page navbar

--- src/views/components/navigation/PageNavbar.jsx
+++ src/views/components/navigation/PageNavbar.jsx
@@ -1,4 +1,5 @@
 import BackControl from "../utility/BackControl";
+import LangSwitcher from "../utility/LangSwitcher";
 import ProfileControl from "../utility/ProfileControl";
 import React from "react";
 
@@ -10,16 +11,23 @@ export default function PageNavbar(props) {
                     <ul className="navbar-nav me-auto">
                         <li className="nav-item">
                             <BackControl />
-                        </li>
-                    </ul>
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item my-auto">
                             <span className="fs-5 fw-semibold text-white text-center">{props.navTitle}</span>
                         </li>
-                        <li className="nav-item dropdown mt-2 mt-sm-0 ms-2">
-                            <ProfileControl />
-                        </li>
                     </ul>
+                    <button className="btn btn-outline-light d-inline-block d-md-none ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i className="fa-solid fa-ellipsis-vertical"></i>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto gap-2">
+                            <li className="nav-item my-auto">
+                                <LangSwitcher />
+                            </li>
+                            <li className="nav-item dropdown mt-2 mt-sm-0 ms-2">
+                                <ProfileControl />
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         </>
