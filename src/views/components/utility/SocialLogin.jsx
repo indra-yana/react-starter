632a98a7326965d55fe8316a5f4d24ace69588cd
Indra Muliana <indra.ndra26@gmail.com>
2022-12-25 16:39:57 +0700

    Improve social login

--- src/views/components/utility/SocialLogin.jsx
+++ src/views/components/utility/SocialLogin.jsx
@@ -3,21 +3,29 @@ import { useTranslation } from "react-i18next"
 export default function SocialLogin(props) {
     const { t } = useTranslation();
 
-    return(
+    return (
         <>
             <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                <p className="lead fw-normal mb-0 me-3">{t('label.sign_in_with')}</p>
-                <button type="button" className="btn btn-primary btn-floating mx-1">
-                    <i className="fab fa-facebook-f text-white"></i>
-                </button>
+                <div className="d-flex flex-column flex-md-row align-items-center gap-3">
+                    <p className="fw-normal mb-0">{t('label.sign_in_with')}</p>
+                    <div className="d-flex gap-3">
+                        <a href="#" class="btn btn-outline-danger btn-social btn-circle shadow-sm" title="Google">
+                            <i class="fab fa-google"> </i>
+                        </a>
 
-                <button type="button" className="btn btn-primary btn-floating mx-1">
-                    <i className="fab fa-twitter text-white"></i>
-                </button>
+                        <a href="#" class="btn btn-outline-primary btn-social btn-circle shadow-sm" title="Facebook">
+                            <i class="fab fa-facebook"></i>
+                        </a>
 
-                <button type="button" className="btn btn-primary btn-floating mx-1">
-                    <i className="fab fa-linkedin-in text-white"></i>
-                </button>
+                        <a href="#" class="btn btn-outline-info btn-social btn-circle shadow-sm" title="Twitter">
+                            <i class="fab fa-twitter"> </i>
+                        </a>
+
+                        <a href="#" class="btn btn-outline-secondary btn-social btn-circle shadow-sm" title="Github">
+                            <i class="fab fa-github"> </i>
+                        </a>
+                    </div>
+                </div>
             </div>
 
             <div className="divider d-flex align-items-center my-4">
