0a09ef3d116a6a165d90b6f7ec52cc21f7a12b6e
Indra Muliana <indra.ndra26@gmail.com>
2023-06-14 18:43:13 +0700

    remove google oauth

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,13 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'src/lang/i18n';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { RouterProvider } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import router from './router';
 
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-			<RouterProvider router={router} />
-		</GoogleOAuthProvider>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 )
